36d0446cac8e58a355ac0ccc9d69fd99cf8420c0
Aythommie <aythommie007@gmail.com>
2022-12-10 15:40:28 -0800

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h>                                                /**                                                                *main- Entry         
- *  
- * Return: Always 0     
- */                                                                int main(void)                                                    {                                                                    puts("\"Programming is like building a multi lingual puzzle");     return (0);
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like a multilingual puzzle");
+return (0);
 }
